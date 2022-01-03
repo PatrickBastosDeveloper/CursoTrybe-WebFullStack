739379b23fcf468ec4daf59f912dea20c04e03ca
Patrick <patrickbastosc@gmail.com>
2022-01-03 16:33:26 -0300

    Exercicios do course

---
+++ Fundamentos_Do_Desenvolvimento_Web/bl08 - Higher_Order_Functions_do_Javascript_Es6/03-_Javascript_Es6_-_Higher_Order_Functions_-_Map_E_Filter/aulaAoVivo/script.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4, 5];
+
+// const newArr = numbers.filter( (item)=> {return item < 4} );
+
+// ou
+
+const newArr = numbers.filter( item=> {return item < 4} );
+
+console.log('newArr', newArr);
