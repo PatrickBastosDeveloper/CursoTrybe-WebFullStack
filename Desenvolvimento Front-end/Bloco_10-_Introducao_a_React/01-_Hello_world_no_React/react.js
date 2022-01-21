7b605c5e335034252bc52b59b5a9c81bc175f94e
Patrick <patrickbastosc@gmail.com>
2022-01-21 16:21:39 -0300

    docs: Atualização de conteúdo bl10.1

---
+++ Desenvolvimento Front-end/Bloco_10-_Introducao_a_React/01-_Hello_world_no_React/react.js
@@ -0,0 +1,22 @@
+// O React é uma das bibliotecas JavaScript de criação de interfaces mais utilizadas no mundo. Ela foi criada e é mantida pelo próprio Facebook .
+
+// Vamos ver o que diferencia React de JavaScript e depois vamos ver um pouco sobre classes. Ao final do dia vamos ter todas as informações para codar um componente básico em React !
+
+// Como dito anteriormente, o React é a principal biblioteca de construção de UI's atualmente. Mas por quê? Quando falamos em React estamos geralmente nos referindo a todo o ecossistema que o envolve:
+// React
+// JSX
+// Babel
+// Webpack
+// Flux/Redux
+// fetch
+// Jest/RTL
+
+// A lista acima mostra algumas das tecnologias que fazem parte desse ecossistema e que te permitem criar aplicações Web de alta qualidade.
+
+
+
+// 
+// A primeira coisa a se saber é que npm (Node Package Manager) é o gerenciador de pacotes em si, ou seja, ele é quem será utilizado para instalar os pacotes em nossas aplicações React, enquanto o npx executa o comando de um pacote sem instalá-lo em si.
+
+// Como o create-react-app é um pacote que a única coisa que faz é criar todos os arquivos necessários para termos um app React, não rodamos ele mais de uma vez por projeto, nem precisamos do pacote instalado em nossas máquinas, por isso SEMPRE executamos ele com o npx e não com o npm .
+
--- Fundamentos_Do_Desenvolvimento_Web/bl09 - Javascript_e_Testes_Assincronos/02-_Javascript_Assincrono_-_Fetch_API_E_Async-Await/course/asyncEAwait.js
+++ Fundamentos_Do_Desenvolvimento_Web/bl09 - Javascript_e_Testes_Assincronos/02-_Javascript_Assincrono_-_Fetch_API_E_Async-Await/course/asyncEAwait.js
@@ -24,10 +24,10 @@ const fetch = require('node-fetch');
 const fetchJoke = async () => {
   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
 
-  const result = await fetch(url)
-    .then((response) => response.json())
-    .then((data) => data.value)
-    .catch((error) => `Algo deu errado :( \n${error}`);
+  const result = await fetch( url )
+    .then( ( response ) => response.json() )
+    .then( ( data ) => data.value )
+    .catch( ( error ) => `Algo deu errado :( \n${ error }` );
   
   console.log(result);
 };
