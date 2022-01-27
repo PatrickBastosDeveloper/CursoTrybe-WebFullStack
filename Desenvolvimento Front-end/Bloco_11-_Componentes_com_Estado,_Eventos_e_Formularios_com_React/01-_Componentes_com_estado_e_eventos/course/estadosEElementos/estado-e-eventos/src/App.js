8a9fbc4edabafc901085d93abe2a8c90b38376a5
Patrick <patrickbastosc@gmail.com>
2022-01-27 18:53:44 -0300

    doc: incluído resumo do conteúdo estado e eventos

--- Desenvolvimento Front-end/Bloco_11-_Componentes_com_Estado,_Eventos_e_Formularios_com_React/01-_Componentes_com_estado_e_eventos/course/estadosEElementos/estado-e-eventos/src/App.js
+++ Desenvolvimento Front-end/Bloco_11-_Componentes_com_Estado,_Eventos_e_Formularios_com_React/01-_Componentes_com_estado_e_eventos/course/estadosEElementos/estado-e-eventos/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class App extends React.Component{
   constructor() { // Quando o React"por trás dos panos" vai renderizar o elemento ele executa uma série de funções, e uma dela é a constructor(). Dessa forma é possível redeclará-la e adionar um comportamento que nos queremos.
-    super()//Mas para isso é necessário escrever a função super() que vai permitir estender o comportamento intern da função contructor seguindo a sequinte orde: Primeiro ele faz a lógica interna dele, em sequida a que adicionamos para só depois renderizar com a função render().      
+    super()//Mas para isso é necessário escrever a função super() que vai permitir estender o comportamento interno da função contructor seguindo a sequinte orde: Primeiro ele faz a lógica interna dele, em sequida a que adicionamos para só depois renderizar com a função render().      
     this.handleClick = this.handleClick.bind(this); // O this é o objeto que o React cria e entrega de volta tudo que se refere aquele componente.
     // O bind() foi feito para atender uma limitação existente no javaScript, que não foi feito para ter classes. Ele permite que a função render() consiga enxergar o this quando ele é chamado dentro de um eventListener (linha 20)
     this.state = {
@@ -30,3 +30,17 @@ export default App;
 // Pelos princípios do React você nunca deve tentar alterar o que um componente recebe como props como no exemplo abaixo:
 
 // this.props.name = 'novo nome';
+
+// Em resumo
+// Vamos recapitular? Hoje, no conteúdo, começamos a desenvolver as seguintes habilidades:
+// Ler o estado de um componente e usá-lo para alterar o que exibimos no browser;
+// Inicializar um componente, dando a ele um estado pré-definido;
+// Atualizar o estado de um componente.
+// Capturar eventos utilizando a sintaxe do React
+// Para isso estudamos, no conteúdo, os seguintes pontos:
+// Todo componente possui seu próprio estado e tem 100% de controle sobre ele;
+// Quando um componente é colocado na tela ele executa uma função chamada constructor , e usando a linha super() podemos sobrescrevê-la para estender seu comportamento;
+// O objeto this , acessível a toda classe de componente React , contém tudo o que concerne àquele componente e deve ser vinculado explicitamente às funções criadas dentro da classe, através da sintaxe this.minhaFuncao = this.minhaFuncao.bind(this) no construtor do componente;
+// Funções que tratam eventos devem ser vinculadas aos seus respectivos elementos com {this.minhaFuncao} ou {() => this.minhaFuncao('Meu parametro') .
+// É possível ler o estado de um componente via this.state , é possível definir um estado inicial no construtor com uma atribuição a this.state e deve-se atualizar tal estado somente com a função this.setState .
+// A atualização do estado é assíncrona e, por isso, se você quiser garantir que uma atualização ocorrerá depois da outra, tal atualização deverá ser definida via callback passada à função this.setState , no formato this.setState((estadoAnterior, props) => novoEstado)
