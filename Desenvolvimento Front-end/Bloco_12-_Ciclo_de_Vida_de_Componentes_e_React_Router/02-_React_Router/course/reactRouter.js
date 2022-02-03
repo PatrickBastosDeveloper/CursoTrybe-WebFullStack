3f128b5c90fa97b88a8719b126d0ad085c184247
Patrick <patrickbastosc@gmail.com>
2022-02-03 06:54:39 -0300

    doc: Atual 12.2 switch

--- Desenvolvimento Front-end/Bloco_12-_Ciclo_de_Vida_de_Componentes_e_React_Router/02-_React_Router/course/reactRouter.js
+++ Desenvolvimento Front-end/Bloco_12-_Ciclo_de_Vida_de_Componentes_e_React_Router/02-_React_Router/course/reactRouter.js
@@ -92,3 +92,20 @@ class App extends Component {
 // /movies/1 ;
 // /movies/2 ;
 // /movies/thor .
+
+// Componente Switch
+
+// O componente Switch é usado para encapsular um conjunto de rotas que você definiu via Route.
+
+// Dada a URL atual da aplicação, o Switch procura de cima para baixo pelo primeiro Route que possuir correspondência entre seu caminho definido na prop path do componente e a URL atual da aplicação.
+
+<Switch>
+  <Route path="/about" component={About} />
+  <Route path="/contact" component={Contact} />
+  <Route path="/movies" component={Movies} />
+  <Route path="/" component={Home} />
+</Switch>
+
+// Ao mudarmos a URL da aplicação para que seu caminho seja /contact , somente o componente Contact será renderizado.
+
+// Todos os filhos de um Switch devem ser Route ou Redirect . Apenas o primeiro filho que corresponder ao local atual será renderizado. Se não houvesse o Switch mais de um componente poderia ser renderizado na mesma rota de forma errada.
