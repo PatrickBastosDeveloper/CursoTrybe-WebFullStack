a92994ce7e7b731519c9d76df045efe5529bc1e7
Patrick <patrickbastosc@gmail.com>
2022-02-03 06:50:34 -0300

    doc: Atual 12.2 passagem de parâm rotas dinâmicas

--- Desenvolvimento Front-end/Bloco_12-_Ciclo_de_Vida_de_Componentes_e_React_Router/02-_React_Router/course/reactRouter.js
+++ Desenvolvimento Front-end/Bloco_12-_Ciclo_de_Vida_de_Componentes_e_React_Router/02-_React_Router/course/reactRouter.js
@@ -85,3 +85,10 @@ class App extends Component {
 // Vale Ressaltar:
 
 // Tanto component quanto render permitem que você tenha acesso a informações de rota ( match , location e history ) via props do componente que você está mapeando. Ou seja, se você tem a rota <Route path="/about" component={About} /> , About terá match , location e history setadas via props.
+
+// Componentes Route com passagem de parâmetro (rotas dinâmicas)
+
+// O interessante em rotas dinâmicas é que podemos utilizar o mesmo componente para renderizar vários caminhos diferentes. Para fazer uso de parâmetro de URL em Route , é preciso fazer uso da sintaxe :nome_do_parametro dentro da propriedade path . Ou seja, <Route path="/movies/:movieId" component={Movie} /> vai definir um parâmetro chamado movieID , e o componente Movie é mapeado a qualquer um dos seguintes caminhos de URL :
+// /movies/1 ;
+// /movies/2 ;
+// /movies/thor .
