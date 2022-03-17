d525573244e97d31a20da8ad1d06f210990bceb4
Patrick <patrickbastosc@gmail.com>
2022-03-17 09:25:38 -0300

    ci: Atualização de conteúdo bl 17.2

--- Desenvolvimento Front-end/Bloco_17-_Context_API_e_React_Hooks/02-_React_Hooks_-_useState_e_useContext/course/videoAula/todo-list/src/App.js
+++ Desenvolvimento Front-end/Bloco_17-_Context_API_e_React_Hooks/02-_React_Hooks_-_useState_e_useContext/course/videoAula/todo-list/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  
+  const addTodo = (newTodo) => {
+      setTodos(todos.concat(newTodo))
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <TodoInput addTodo={ addTodo } />
+      <TodoList todos={ todos}/>
+    </main>
+  )
 }
 
 export default App;
---
+++ Desenvolvimento Front-end/Bloco_17-_Context_API_e_React_Hooks/02-_React_Hooks_-_useState_e_useContext/course/videoAula/todo-list/src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TodoList ({todos}){
+  return (
+    <ul>
+     { todos.map( todo => <li>{ todo }</li>)}
+    </ul>
+ ) 
+}
+
+export default TodoList;
